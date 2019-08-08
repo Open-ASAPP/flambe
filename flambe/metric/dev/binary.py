e46a6af5c5ed2fe71b89f83eddb935707cd17067
Ivan Itzcovich <iitzcovich@asapp.com>
2019-08-08 16:39:13 -0300

    Fix bugs and update torch to 1.2

--- flambe/metric/dev/binary.py
+++ flambe/metric/dev/binary.py
@@ -39,8 +39,8 @@ class BinaryMetric(Metric):
         # Cast because pytorch's byte method returns a Tensor type
         pred = pred.squeeze()
         target = target.squeeze()
-        pred = (pred > self.threshold).byte()
-        target = target.byte()
+        pred = (pred > self.threshold).to(torch.bool)
+        target = target.to(torch.bool)
 
         return self.compute_binary(pred, target)
 

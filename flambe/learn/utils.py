e883d2c35fded4a7c9654656900f2217e3c877b1
ciguaran@asapp.com <ciguaran@asapp.com>
2019-10-31 13:38:52 -0300

    CRU

--- flambe/learn/utils.py
+++ flambe/learn/utils.py
@@ -1,7 +1,12 @@
 import torch
 
 
-def select_device(device):
+def select_device(device: str) -> str:
+    """
+    Chooses the torch device to run in.
+    :return: the passed-as-parameter device if any, otherwise
+    cuda if available. Last option is cpu.
+    """
     if device is not None:
         return device
     else:

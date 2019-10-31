46db2a9fee41f75e95bf60d05284bc8b6aa8efb1
ciguaran@asapp.com <ciguaran@asapp.com>
2019-10-31 12:20:45 -0300

    fixing linter

--- flambe/learn/utils.py
+++ flambe/learn/utils.py
@@ -1,8 +1,7 @@
 import torch
 
-
 def select_device(device):
     if device is not None:
         return device
     else:
-        return  "cuda" if torch.cuda.is_available() else "cpu"
+        return "cuda" if torch.cuda.is_available() else "cpu"

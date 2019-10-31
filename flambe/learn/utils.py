8d9703d503c74be27e06420d1a7f55390fe9b16e
ciguaran@asapp.com <ciguaran@asapp.com>
2019-10-31 13:46:41 -0300

    styling comment

--- flambe/learn/utils.py
+++ flambe/learn/utils.py
@@ -6,9 +6,19 @@ import torch
 def select_device(device: Union[torch.device, str]) -> Union[torch.device, str]:
     """
     Chooses the torch device to run in.
-    :return: the passed-as-parameter device if any, otherwise
-    cuda if available. Last option is cpu.
+
+     Parameters
+       ------------
+     device: Union[torch.device, str]
+         A device or a string representing a device, such as 'cpu'
+
+     Returns
+     ---------
+     str
+         the passed-as-parameter device if any, otherwise
+         cuda if available. Last option is cpu.
     """
+
     if device is not None:
         return device
     else:

3a9c6bd0f13656e7d0538c4969dc1aa54ef7dcec
Jeremy Wohlwend <jeremy@asapp.com>
2019-12-10 18:47:20 -0500

    remove optim module

--- flambe/nlp/transformers/__init__.py
+++ flambe/nlp/transformers/__init__.py
@@ -1,8 +1,5 @@
 from flambe.nlp.transformers.field import PretrainedTransformerField
 from flambe.nlp.transformers.model import PretrainedTransformerEmbedder
-from flambe.nlp.transformers.optim import AdamW, ConstantLRSchedule
-from flambe.nlp.transformers.optim import WarmupConstantSchedule, WarmupLinearSchedule
 
 
-__all__ = ['PretrainedTransformerField', 'PretrainedTransformerEmbedder',
-           'AdamW', 'ConstantLRSchedule', 'WarmupConstantSchedule', 'WarmupLinearSchedule']
+__all__ = ['PretrainedTransformerField', 'PretrainedTransformerEmbedder']
